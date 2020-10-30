cbea5983724d5bdc858a8db81b2f88750fbc394c
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-10-30 19:22:37 -0400

    filter added

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -5,6 +5,7 @@ export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
   state = {
     plants: [],
+    copy: [],
   };
 
   // when the component mounts:
@@ -16,6 +17,7 @@ export default class PlantList extends Component {
       .then((resp) =>
         this.setState({
           plants: [...resp.data.plantsData],
+          copy: [...resp.data.plantsData],
         })
       )
       .catch((err) => {
@@ -23,32 +25,60 @@ export default class PlantList extends Component {
       });
   }
 
+  onClick = (e) => {
+    if (e.target.value === "all") {
+      this.setState({
+        plants: [...this.state.copy],
+      });
+    } else {
+      this.setState({
+        plants: this.state.copy.filter((plant) => {
+          return plant.difficulty === e.target.value;
+        }),
+      });
+    }
+  };
+
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
-      <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
-          <div data-testid="plant-card" className="plant-card" key={plant.id}>
-            <img className="plant-image" src={plant.img} alt={plant.name} />
-            <div className="plant-details">
-              <h2 className="plant-name">{plant.name}</h2>
-              <p className="plant-scientific-name">{plant.scientificName}</p>
-              <p>{plant.description}</p>
-              <div className="plant-bottom-row">
-                <p>${plant.price}</p>
-                <p>☀️ {plant.light}</p>
-                <p>💦 {plant.watering}x/month</p>
-              </div>
-              <button
-                className="plant-button"
-                onClick={() => this.props.addToCart(plant)}
-              >
-                Add to cart
+      <>
+        <div className="links-heading">
+          <h3>Sort by difficulty</h3>
+        </div>
+        <div className="links">
+          {["easy", "medium", "all"].map((item) => {
+            return (
+              <button onClick={this.onClick} value={item}>
+                {item}
               </button>
+            );
+          })}
+        </div>
+        <main className="plant-list">
+          {this.state?.plants?.map((plant) => (
+            <div data-testid="plant-card" className="plant-card" key={plant.id}>
+              <img className="plant-image" src={plant.img} alt={plant.name} />
+              <div className="plant-details">
+                <h2 className="plant-name">{plant.name}</h2>
+                <p className="plant-scientific-name">{plant.scientificName}</p>
+                <p>{plant.description}</p>
+                <div className="plant-bottom-row">
+                  <p>${plant.price}</p>
+                  <p>☀️ {plant.light}</p>
+                  <p>💦 {plant.watering}x/month</p>
+                </div>
+                <button
+                  className="plant-button"
+                  onClick={() => this.props.addToCart(plant)}
+                >
+                  Add to cart
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
-      </main>
+          ))}
+        </main>
+      </>
     );
   }
 }
